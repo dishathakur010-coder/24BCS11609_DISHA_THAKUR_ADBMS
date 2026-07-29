66bb90c9214a3212fb1bd9f60795fbbe1e6e15f0
dishathakur010-coder <dishathakur010@gmail.com>
2026-07-29 10:58:43 +0530

    Add SQL queries for employee and customer data
    Added SQL queries to display employee-manager relationships and customer-product combinations.

---
+++ Experiments/Experiment_4/4.3/Code.sql
@@ -0,0 +1,10 @@
+https://www.codechef.com/viewsolution/1321428246
+
+-- 1.Employee and Manager Names: Display a list of employee names along with their manager's names. Use the 'employees' table provided.
+
+-- 2.Every Possible Combination: Show every possible combination of 'customer_name' from the 'customers' table and 'product_name' from the 'products' table.
+SELECT e1.employee_name AS Employee,e2.employee_name AS Manager FROM employees AS e1
+LEFT JOIN employees AS e2
+ON e1.manager_id=e2.employee_id;
+
+SELECT c1.customer_name,p1.product_name FROM customers as c1 CROSS JOIN products as p1;
